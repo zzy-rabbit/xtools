2a91af8db85bf5b6daff2ec192218d6f66d2666e
zzy <>
2026-01-05 11:36:27 +0800

    v0.0.4

--- plugins/encrypt/plugin.go
+++ plugins/encrypt/plugin.go
@@ -1,1 +1,17 @@
-ÖØÃüÃû "E:\code\xtools\plugins\encrypt\plugin.go1" -
+package encrypt
+
+import (
+	"github.com/zzy-rabbit/xtools/plugins/encrypt/internal"
+	"github.com/zzy-rabbit/xtools/xcontext"
+	"github.com/zzy-rabbit/xtools/xerror"
+	"github.com/zzy-rabbit/xtools/xplugin"
+)
+
+func init() {
+	ctx := xcontext.Background()
+	plugin := internal.New(ctx)
+	err := xplugin.Register(ctx, plugin)
+	if xerror.Error(err) {
+		panic(err)
+	}
+}
