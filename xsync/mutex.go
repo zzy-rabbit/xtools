319706225ba491dae20f55ae0791f5e944950f9f
zzy <chongya444@gmail.com>
2026-04-07 20:32:01 +0800

    xsync

--- xsync/mutex.go
+++ xsync/mutex.go
@@ -6,37 +6,37 @@ import (
 	"sync/atomic"
 )
 
-type XMutex struct {
+type Mutex struct {
 	mu sync.RWMutex
 
 	readers int32 // 当前读锁数量
 	writer  int32 // 是否有写锁（0/1）
 }
 
-func (m *XMutex) RLock(ctx context.Context) {
+func (m *Mutex) RLock(ctx context.Context) {
 	m.mu.RLock()
 	atomic.AddInt32(&m.readers, 1)
 }
 
-func (m *XMutex) RUnlock(ctx context.Context) {
+func (m *Mutex) RUnlock(ctx context.Context) {
 	atomic.AddInt32(&m.readers, -1)
 	m.mu.RUnlock()
 }
 
-func (m *XMutex) Lock(ctx context.Context) {
+func (m *Mutex) Lock(ctx context.Context) {
 	m.mu.Lock()
 	atomic.StoreInt32(&m.writer, 1)
 }
 
-func (m *XMutex) Unlock(ctx context.Context) {
+func (m *Mutex) Unlock(ctx context.Context) {
 	atomic.StoreInt32(&m.writer, 0)
 	m.mu.Unlock()
 }
 
-func (m *XMutex) Locked(ctx context.Context) bool {
+func (m *Mutex) Locked(ctx context.Context) bool {
 	return atomic.LoadInt32(&m.writer) == 1 || atomic.LoadInt32(&m.readers) > 0
 }
 
-func (m *XMutex) RLocked(ctx context.Context) bool {
+func (m *Mutex) RLocked(ctx context.Context) bool {
 	return atomic.LoadInt32(&m.readers) > 0
 }
